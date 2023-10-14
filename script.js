2a4238ca900bca9fd855a47d9a5882b38c224e2a
MTCodes01 <koshi9866@gmail.com>
2023-10-14 11:35:04 +0530

    Too many changes to specify!

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // JavaScript code
+// Get the elements by their IDs
+const emailText = document.getElementById('email-text');
+const emailInput = document.getElementById('email-input');
+const registerButton = document.getElementById('register-button');
+
+// Add a click event listener to the emailText element
+emailText.addEventListener('click', function () {
+  emailText.style.display = 'none'; // Hide the text
+  emailInput.style.display = 'block'; // Show the input field
+});
+
+// You can add an event listener to the button for further actions, e.g., handling registration.
+registerButton.addEventListener('click', function () {
+  const userEmail = emailInput.value; // Get the entered email
+  // Perform registration or any other action you need here.
+});
